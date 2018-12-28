761d462200389319143e3c0bda7bbc71adcab1be
zbw <zbw0415@163.com>
2018-12-28 14:39:45 +0800

    修复上拉菜单

--- components/ActionSheet/actionsheet.js
+++ components/ActionSheet/actionsheet.js
@@ -4,6 +4,13 @@ Component({
     isShow: {
       type: Boolean,
       value: false,
+      observer: function(newVal, oldVal) {
+        if (newVal === true) {
+          this.setData({
+            isRender: true
+          })
+        }
+      }
     },
     title: {
       type: String,
@@ -16,6 +23,7 @@ Component({
   },
   data: {
     // 这里是一些组件内部数据 
+    isRender: false
   },
   ready: function() {
 
